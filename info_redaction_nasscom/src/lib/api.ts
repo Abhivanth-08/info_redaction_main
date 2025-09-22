2085e5335e83cda5cba0b65eafec7a8bb06e0f5a
DhamodharanT06 <dhamodharant45@gmail.com>
2025-09-22 21:33:07 +0530

    Updated api.ts

--- info_redaction_nasscom/src/lib/api.ts
+++ info_redaction_nasscom/src/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://info-redact-backend.onrender.com" || "http://localhost:8000";
+export const API_BASE_URL = "https://backend-y61r.onrender.com" ;
 
 export type RedactResponse = {
   session_id: string;
