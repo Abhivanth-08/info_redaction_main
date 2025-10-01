4d325a4bc6bfaf050b062824f947b9c3877bb13d
DhamodharanT06 <dhamodharant45@gmail.com>
2025-10-01 20:19:12 +0530

    Update vite.config.js

--- landing/vite.config.js
+++ landing/vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './landing',      // Vite root is landing folder
-  build: {
-    outDir: '../dist',    // Output goes to dist folder
-    emptyOutDir: true,
-  },
 });
